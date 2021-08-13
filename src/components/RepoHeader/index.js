2c36b890206ce35b4212ccddf96b1c543c535940
Mohsen Tayseer <88701752+mohsentayseer@users.noreply.github.com>
2021-08-13 22:30:59 +0200

    Add files via upload

---
+++ src/components/RepoHeader/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text, Image } from "react-native";
+import styles from "./styles";
+import logo from "../../../assets/logo.png";
+
+function RepoHeader({ imageSrc, text, containerStyle, imageStyle, textStyle }) {
+  return (
+    <View style={[styles.container, containerStyle]}>
+      <Image
+        source={imageSrc ? { uri: imageSrc } : logo}
+        style={styles.image}
+      />
+      <Text style={[styles.text, textStyle]}>{text}</Text>
+    </View>
+  );
+}
+
+export { RepoHeader };
